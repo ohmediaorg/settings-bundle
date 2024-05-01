a22cd28b80b8c50a56b44203afc831bac0e77a73
Justin Hoffman <justin@ohmedia.ca>
2024-05-01 12:13:27 -0600

    update entity syntax

--- src/Entity/Setting.php
+++ src/Entity/Setting.php
@@ -2,36 +2,37 @@
 
 namespace OHMedia\SettingsBundle\Entity;
 
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity]
 class Setting
 {
-    #[ORM\Id()]
-    #[ORM\Column(type: 'string', length: 255)]
-    private $id;
+    #[ORM\Id]
+    #[ORM\Column(length: 255)]
+    private string $id;
 
-    #[ORM\Column(type: 'text', nullable: true)]
-    private $value;
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $value = null;
 
-    public function getId()
+    public function getId(): string
     {
         return $this->id;
     }
 
-    public function setId(string $id)
+    public function setId(string $id): static
     {
         $this->id = $id;
 
         return $this;
     }
 
-    public function getValue()
+    public function getValue(): ?string
     {
         return $this->value;
     }
 
-    public function setValue($value)
+    public function setValue($value): static
     {
         $this->value = $value;
 
